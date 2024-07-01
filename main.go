58928e472169a3ca509c14375956517ec25fa531
IAMME543 <152034114+IAMME543@users.noreply.github.com>
2024-07-01 15:48:25 +1000

    Autosaved notes and set up with fyne

---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/widget"
+)
+
+func main() {
+
+	a := app.New()
+	w := a.NewWindow("Text Editor")
+
+	windowSize := fyne.NewSize(700, 750)
+
+	inputFeild := widget.NewEntry()
+	inputFeild.MultiLine = true
+	inputFeild.Text = LoadSavedNote()
+
+	inputFeild.OnChanged = func(text string) {
+		SaveFile(text)
+	}
+
+	w.SetContent(inputFeild)
+
+	w.Resize(windowSize)
+	w.CenterOnScreen()
+
+	w.ShowAndRun()
+
+}
+
+func SaveFile(text string) {
+	file, err := os.Create("Note.txt")
+	if err != nil {
+		log.Print("Error creating file 'Note.txt'")
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(text)
+
+	if err != nil {
+		fmt.Println("Error writing to file:", err)
+		return
+	}
+
+}
+
+func LoadSavedNote() string {
+	file, err := os.Open("Note.txt")
+	if err != nil {
+		log.Print("There was an error opening saved note")
+	}
+	defer file.Close()
+
+	var content string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		content += scanner.Text() + "\n"
+	}
+	return content
+
+}
